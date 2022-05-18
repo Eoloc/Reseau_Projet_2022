3935401d01c73cdd358e60ad5b1909ac16421ca8
Alexandre Sousa <eoloc57@gmail.com>
2022-05-18 16:34:32 +0200

    First commit

---
+++ src/main/java/fr/ul/miage/Reseau_Projet_2022/Controllers/ServerController.java
@@ -0,0 +1,5 @@
+package fr.ul.miage.Reseau_Projet_2022.Controllers;
+
+public class ServerController {
+    public ServerController(){ }
+}
---
+++ src/main/java/fr/ul/miage/Reseau_Projet_2022/Main.java
@@ -0,0 +1,38 @@
+package fr.ul.miage.Reseau_Projet_2022;
+
+import fr.ul.miage.Reseau_Projet_2022.Models.WebSocketServer;
+import org.glassfish.tyrus.server.Server;
+
+import javax.websocket.DeploymentException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) {
+        runServer();
+    }
+
+    private static void runServer() {
+        Server webSocketServer = null;
+        try {
+            webSocketServer = new Server("127.0.0.1", 9999, "/websocket",  WebSocketServer.class);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                webSocketServer.start();
+            } catch (DeploymentException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Please press a key to stop the server.");
+            reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (webSocketServer != null) {
+                webSocketServer.stop();
+            }
+        }
+    }
+}
