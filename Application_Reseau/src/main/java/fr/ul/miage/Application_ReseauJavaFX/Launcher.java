96089f689203bc556bcbbf40f2c691d9e36f3adf
Alexandre Sousa <eoloc57@gmail.com>
2022-05-22 14:31:40 +0200

    Ajout de la class CLientEndpoint et creation du websocket avec le serveur

--- Application_Reseau/src/main/java/fr/ul/miage/Application_ReseauJavaFX/Launcher.java
+++ Application_Reseau/src/main/java/fr/ul/miage/Application_ReseauJavaFX/Launcher.java
@@ -1,12 +1,25 @@
 package fr.ul.miage.Application_ReseauJavaFX;
 
+import fr.ul.miage.Application_Reseau.ClientEndpoint;
+import org.glassfish.tyrus.client.ClientManager;
+
+import javax.websocket.DeploymentException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.CountDownLatch;
+
 public class Launcher {
 	
-	public Launcher() {
-	
-	}
-	
 	public static void main(String[] args) {
-		App.main(args);
+
+		ClientManager client = ClientManager.createClient();
+		try {
+			URI uri = new URI("ws://127.0.0.1:9999/");
+			client.connectToServer(ClientEndpoint.class, uri);
+			App.main(args);
+		} catch (DeploymentException | URISyntaxException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
